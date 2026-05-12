7e122d474e6c7bfe287c61cc5916bc989dab490a
MonkeySlayer67 <florusaron@gmail.com>
2026-05-12 14:03:40 +0200

    Update AssemblyInfo with new informational version and cache hash

--- MyFirstRepository/obj/Debug/net9.0/MyFirstRepository.AssemblyInfo.cs
+++ MyFirstRepository/obj/Debug/net9.0/MyFirstRepository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstRepository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4f93326d89ad38d25a96778a0d9b6a3956f6db")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstRepository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstRepository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
